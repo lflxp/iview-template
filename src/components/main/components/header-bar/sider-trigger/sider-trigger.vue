55912abab8d06a057c80ec6908162753ef79dde7
lflxp <lxp@lflxp.cn>
2019-12-04 01:21:04 +0800

    add tooltip on button

--- src/components/main/components/header-bar/sider-trigger/sider-trigger.vue
+++ src/components/main/components/header-bar/sider-trigger/sider-trigger.vue
@@ -1,42 +1,25 @@
 <template>
-  <Tooltip placement="bottom-start">
+  <Tooltip placement="bottom-start" theme="light">
     <a @click="handleChange" type="text" :class="['sider-trigger-a', collapsed ? 'collapsed' : '']"><Icon :type="icon" :size="size" /></a>
     <div slot="content" style="width: 666px;">
-      <p>子元素向左排列</p>
-      <Row type="flex" justify="start" class="code-row-bg">
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-      </Row>
-      <p>子元素向右排列</p>
-      <Row type="flex" justify="end" class="code-row-bg">
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-      </Row>
-      <p>子元素居中排列</p>
-      <Row type="flex" justify="center" class="code-row-bg">
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-      </Row>
-      <p>子元素等宽排列</p>
-      <Row type="flex" justify="space-between" class="code-row-bg">
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-      </Row>
-      <p>子元素分散排列</p>
-      <Row type="flex" justify="space-around" class="code-row-bg">
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-          <Col span="4">col-4</Col>
-      </Row>
+      <Button type="default" ghost>Default</Button>
+      <Button type="primary" ghost>Primary</Button>
+      <Button type="dashed" ghost>Dashed</Button>
+      <Button type="text" ghost>Text</Button>
+      <Button type="info" ghost>Info</Button>
+      <Button type="success" ghost>Success</Button>
+      <Button type="warning" ghost>Warning</Button>
+      <Button type="error" ghost>Error</Button>
+      <br/>
+      <Button>Default</Button>
+      <Button type="primary">Primary</Button>
+      <Button type="dashed">Dashed</Button>
+      <Button type="text">Text</Button>
+      <br><br>
+      <Button type="info">Info</Button>
+      <Button type="success">Success</Button>
+      <Button type="warning">Warning</Button>
+      <Button type="error">Error</Button>
     </div>
   </Tooltip>
 </template>
