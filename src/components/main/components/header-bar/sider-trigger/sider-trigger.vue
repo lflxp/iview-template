85f1c9f2f8fb6086646715e740269a0672f05bd5
lflxp <lxp@lflxp.cn>
2019-12-04 01:12:48 +0800

    add tooltip on button

--- src/components/main/components/header-bar/sider-trigger/sider-trigger.vue
+++ src/components/main/components/header-bar/sider-trigger/sider-trigger.vue
@@ -1,5 +1,44 @@
 <template>
-  <a @click="handleChange" type="text" :class="['sider-trigger-a', collapsed ? 'collapsed' : '']"><Icon :type="icon" :size="size" /></a>
+  <Tooltip placement="bottom-start">
+    <a @click="handleChange" type="text" :class="['sider-trigger-a', collapsed ? 'collapsed' : '']"><Icon :type="icon" :size="size" /></a>
+    <div slot="content" style="width: 666px;">
+      <p>子元素向左排列</p>
+      <Row type="flex" justify="start" class="code-row-bg">
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+      </Row>
+      <p>子元素向右排列</p>
+      <Row type="flex" justify="end" class="code-row-bg">
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+      </Row>
+      <p>子元素居中排列</p>
+      <Row type="flex" justify="center" class="code-row-bg">
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+      </Row>
+      <p>子元素等宽排列</p>
+      <Row type="flex" justify="space-between" class="code-row-bg">
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+      </Row>
+      <p>子元素分散排列</p>
+      <Row type="flex" justify="space-around" class="code-row-bg">
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+          <Col span="4">col-4</Col>
+      </Row>
+    </div>
+  </Tooltip>
 </template>
 <script>
 export default {
